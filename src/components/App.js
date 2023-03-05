fcad780b75d20cdbaf68390f008addb31f3b6506
Santosh Kumar Malik <santoshkumarmalik473@gmail.com>
2023-03-05 14:22:13 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,56 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
+import React, { useState } from 'react';
 
-const App = () => {
-  const [rating, setRating] = useState();
-  const [comment, setComment] = useState();
-  const [commentError, setCommentError] = useState();
+function App() {
+  const [rating, setRating] = useState(5);
+  const [comment, setComment] = useState('');
+  const [commentError, setCommentError] = useState('');
+
+  const handleRatingChange = (event) => {
+    setRating(parseInt(event.target.value));
+  };
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+    if (event.target.value.length < 5) {
+      setCommentError('Comment must be at least 5 characters');
+    } else {
+      setCommentError('');
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Rating: ${rating}, Comment: ${comment}`);
+  };
 
   return (
-    <div id="main">
-      <form>
+    <div>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor='rating'>Rating: </label>
-          <input 
+          <label htmlFor="rating">Rating</label>
+          <input
+            id="rating"
             type="range"
             min="1"
             max="10"
-            id="rating"
+            value={rating}
+            onChange={handleRatingChange}
           />
-          <span className='rating'>rating</span>
+          <span className="rating">{rating}</span>
         </div>
         <div>
-          <label htmlFor='comment'>Comment: </label>
-          <textarea 
-            id='comment'
+          <label htmlFor="comment">Comment</label>
+          <textarea
+            id="comment"
+            value={comment}
+            onChange={handleCommentChange}
           />
-          <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>
+          {commentError && <p className="comment-error">{commentError}</p>}
         </div>
-        <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
-  )
+  );
 }
-
 
 export default App;
